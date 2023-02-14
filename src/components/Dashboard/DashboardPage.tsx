d4a1bb3e2dae0e503a16eb55ce3e5e25b71a9c8d
ahmedsaliuGit <saliu.lito@gmail.com>
2023-02-14 15:19:12 +0100

    Create Nav mobile styling

--- src/components/Dashboard/DashboardPage.tsx
+++ src/components/Dashboard/DashboardPage.tsx
@@ -1,37 +1,47 @@
+import { useState } from "react";
 import Logo from "../../assets/images/logo.svg";
 import UserImage from "../../assets/images/user-image.png";
 import "./DashboardPage.css";
 
 function DashboardPage() {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="grid-container">
-      <div className="menu-icon">
-        <i className="fas fa-bars"></i>
-      </div>
       <header className="header">
-        <div className="header__flex-item logo-form">
-          <img src={Logo} alt="Lendsequare logo" />
-          <form action="post">
+        <div className="header__logo">
+          <div className="menu-icon">
+            <i className="fas fa-bars"></i>
+          </div>
+          <img src={Logo} alt="Lendsequare logo" className="logo" />
+          <div className="menu-icon" onClick={() => setNav(!nav)}>
+            {nav ? (
+              <i className="fas fa-close"></i>
+            ) : (
+              <i className="fas fa-bars"></i>
+            )}
+          </div>
+        </div>
+        <div className={`header__nav ${nav && "active"}`}>
+          <form action="post" className="header__search">
             <input type="text" aria-label="search" />
             <button type="button"></button>
           </form>
-        </div>
-        <div className="header__flex-item">
-          <nav>
-            <ul>
-              <li>Doc</li>
-              <li>icon</li>
-              <li>
-                <img src={UserImage} alt="user avatar" /> Adedeji
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="list__item">Doc</li>
+              <li className="list__item">icon</li>
+              <li className="list__item">
+                <img src={UserImage} alt="user avatar" /> <br /> Adedeji
               </li>
             </ul>
           </nav>
         </div>
       </header>
       <aside className="sidenav">
-        <div className="sidenav__close-icon">
+        {/* <div className="sidenav__close-icon">
           <i className="fas fa-times"></i>
-        </div>
+        </div> */}
         <ul className="sidenav__list">
           <li className="list__item">
             <a className="list__link" href="#home">
@@ -40,36 +50,7 @@ function DashboardPage() {
           </li>
         </ul>
       </aside>
-      <main className="dash-main">
-        <div className="main-header">
-          <div className="main-header__heading">Hello User</div>
-          <div className="main-header__updates">Recent Items</div>
-        </div>
-        <div className="main-overview">
-          <div className="overviewcard">
-            <div className="overviewcard__icon">Overview</div>
-            <div className="overviewcard__info">Card</div>
-          </div>
-          <div className="overviewcard">
-            <div className="overviewcard__icon">Overview</div>
-            <div className="overviewcard__info">Card</div>
-          </div>
-          <div className="overviewcard">
-            <div className="overviewcard__icon">Overview</div>
-            <div className="overviewcard__info">Card</div>
-          </div>
-          <div className="overviewcard">
-            <div className="overviewcard__icon">Overview</div>
-            <div className="overviewcard__info">Card</div>
-          </div>
-        </div>
-        <div className="main-cards">
-          <div className="card">Card</div>
-          <div className="card">Card</div>
-          <div className="card">Card</div>
-        </div>
-      </main>
-      <footer className="footer"></footer>
+      <main className="dash-main"></main>
     </div>
   );
 }
